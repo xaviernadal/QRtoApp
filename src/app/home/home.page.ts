7006169caf5bb712441d621ceadb8f0e27d17707
xaviernadal <xavinadal14@gmail.com>
2022-07-14 09:55:33 +0200

    Android working. iOS start

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import {
   FirebaseDynamicLinks,
   LinkConfig,
@@ -10,17 +11,31 @@ import {
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
+  url_content : string = "";
+  
+  constructor(private alertController: AlertController) {}
   ngOnInit() {
     this.listenToDeepLinkOpen();
   }
 
   listenToDeepLinkOpen() {
     FirebaseDynamicLinks.addListener('deepLinkOpen', (data: { url: string }) => {
-        console.log(data);
+        this.showAlert(data.url);
     });
     
   }
-
+  async showAlert(message: string) {
+    const alert = await this.alertController.create({
+      header: 'Welcome',
+      message: message,
+      buttons: [{
+        text: 'Refresh',
+        handler: () => {
+          window.location.reload();
+        }
+      }],
+    });
+    await alert.present();
+    await alert.onDidDismiss();
+  }
 }
